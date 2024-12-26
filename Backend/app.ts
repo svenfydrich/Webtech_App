7037140513e7023bc1be693c1f56da867f604fa4
Sven Fydrich <sven.fydrich@hm.edu>
2024-12-26 14:27:30 +0100

    rework backend to communicate via axios/cors with frontend

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express';
 import travelRoutes from './routes/travels';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT: number = 1337;
 
 //Configure Middleware and Routes
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api/travels', travelRoutes);
